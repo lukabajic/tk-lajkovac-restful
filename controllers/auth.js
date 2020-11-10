aaa75999d6d6d9923758e2c14e488c9c661a77a1
lukabajic <lukabajic.dev@gmail.com>
2020-11-10 09:58:59 +0100

    Send verifaction email on signup
    Add '@sendgrid/mail' to the project to handle sending verification
    email. Also created ./utility/mailTemplate to keep auth code clean.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,12 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const sendgrid = require("@sendgrid/mail");
 
 const User = require("../models/user");
+
+const html = require("./utility/mailTemplate");
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.register = async (req, res, next) => {
   // extract email and password from request
@@ -33,22 +38,39 @@ exports.register = async (req, res, next) => {
       { expiresIn: "14d" }
     );
 
-    // success response
-    res.status(201).json({
-      status: 201,
-      message: "Korisnik je uspešno kreiran.",
-      token,
-      expiresIn: 14 * 24 * 60 * 60, // token expiration
-      user: {
-        userId: result._id,
-        email: result.email,
-        emailVerified: result.emailVerified,
-        isAdmin: result.isAdmin,
-        displayName: result.displayName,
-        avatarUrl: result.avatarUrl,
-        phone: result.phone,
-      },
-    });
+    // verification email config
+    const message = {
+      to: email,
+      from: "me@lukabajic.dev",
+      subject: "Potvrda email adrese",
+      html: html(result._id),
+    };
+    // verification email send
+    sendgrid
+      .send(message)
+      .then(() => {
+        // success response
+        res.status(201).json({
+          status: 201,
+          message: "Korisnik je uspešno kreiran.",
+          token,
+          expiresIn: 14 * 24 * 60 * 60, // token expiration
+          user: {
+            userId: result._id,
+            email: result.email,
+            emailVerified: result.emailVerified,
+            isAdmin: result.isAdmin,
+            displayName: result.displayName,
+            avatarUrl: result.avatarUrl,
+            phone: result.phone,
+          },
+        });
+      })
+      .catch(() => {
+        error = new Error("Greška pri slanju verifikacionog koda.");
+        error.statusCode = 502;
+        throw error;
+      });
   } catch (err) {
     // error handling
     res.json({
